bec16fac7ad3bb8def45dbb7f0f90e56c9ecabb3
Jon Zielinski <jon.zielinski@illuminerdyit.com>
2025-12-11 19:00:51 -0500

    20251211

--- CREATE TABLE AzureTestData.sql
+++ CREATE TABLE AzureTestData.sql
@@ -1,18 +1,18 @@
 USE [sandbox];
-
-DROP TABLE IF EXISTS [dbo].[AzureTestData];
-
-CREATE TABLE [dbo].[AzureTestData] (
-	[RowId]				BIGINT		  IDENTITY(1, 1) NOT NULL
-   ,[RowInsertDatetime] DATETIME	  NOT NULL CONSTRAINT [DF_AzureTestData_RowInsertDatetime] DEFAULT (GETDATE())
-   ,[RowInsertUser]		NVARCHAR(128) NOT NULL CONSTRAINT [DF_AzureTestData_RowInsertUser] DEFAULT (SUSER_SNAME())
-   ,[RowVersion]		TIMESTAMP	  NOT NULL
-   ,[RowSource]			VARCHAR(1000) NULL
-   ,[Title]				VARCHAR(1000) NULL
-   ,[ReleaseDate]		VARCHAR(1000) NULL
-   ,[RunTime]			VARCHAR(1000) NULL
-   ,[Rating]			VARCHAR(1000) NULL
-   ,CONSTRAINT [PK_AzureTestData] PRIMARY KEY CLUSTERED ([RowId] ASC)
-);
 GO
+DROP TABLE IF EXISTS [dbo].[azure_test_data];
 
+CREATE TABLE [dbo].[azure_test_data] (
+	 [row_id]                  BIGINT NOT NULL IDENTITY(1, 1)
+	,[row_datetime_begin]      DATETIME2 GENERATED ALWAYS AS ROW START /* HIDDEN */ NOT NULL
+	,[row_datetime_end]        DATETIME2 GENERATED ALWAYS AS ROW END HIDDEN NOT NULL
+	,[row_user]                NVARCHAR(128) NOT NULL CONSTRAINT [DF_azure_test_data_row_user] DEFAULT (SUSER_SNAME())
+	,[row_source]              VARCHAR(1000) NOT NULL
+   ,[row_source_line_number]  INT NOT NULL
+	,[title]                   VARCHAR(1000) NULL
+   ,[release_date]            VARCHAR(1000) NULL
+	,[run_time]	               VARCHAR(1000) NULL
+	,[rating]                  VARCHAR(1000) NULL
+	,PERIOD FOR SYSTEM_TIME([row_datetime_begin], [row_datetime_end])
+	,CONSTRAINT [PK_azure_test_data] PRIMARY KEY ([row_id])
+);
